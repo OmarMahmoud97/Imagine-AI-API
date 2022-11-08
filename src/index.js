0770d4b5d520e26fee5577665ace88fd24bbbb3f
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-11-08 15:26:32 +0000

    update cors code

--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@ const express = require("express");
 const app = express();
 const deepai = require("deepai");
 const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
-app.use(cors());
 deepai.setApiKey(process.env.DEEP_AI_API_KEY);
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 app.post("/image", (req, res) => {
   if (!req.body.user_prompt) {
