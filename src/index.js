9de8cd5f2e90e223d11379283dc0192c3f407c0b
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-11-08 15:09:22 +0000

    add cors middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ app.use(express.json());
 app.use(cors());
 deepai.setApiKey(process.env.DEEP_AI_API_KEY);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.post("/image", (req, res) => {
   if (!req.body.user_prompt) {
     return res.status(400).json({
